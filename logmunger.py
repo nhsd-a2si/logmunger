c897877f94ca621cbd756f61a5feaf02f94d800a
Mark Henwood <mcbhenwood@gmail.com>
2018-12-18 22:08:01 +0000

    Make args FileTypes (and fix tests)

--- logmunger.py
+++ logmunger.py
@@ -7,9 +7,11 @@ def parse_args(args):
     parser = argparse.ArgumentParser(
         description='Munger for Service Finder + DoS logs')
     parser.add_argument('--sfslog',
+                        type=argparse.FileType('r'),
                         help='path to text file containing SFS logs',
                         required=True)
     parser.add_argument('--doslog',
+                        type=argparse.FileType('r'),
                         help='path to CSV file containing DoS logs',
                         required=True)
     return parser.parse_args(args)
--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -10,23 +10,25 @@ class ArgParseTestCase(unittest.TestCase):
 
     def test_exit_with_just_sfslog_arg(self):
         with self.assertRaises(SystemExit):
-            logmunger.parse_args(['--sfslog', 'somefile.txt'])
+            logmunger.parse_args(
+                ['--sfslog', 'tests/fixtures/good_sfs_log.txt'])
 
     def test_exit_with_just_doslog_arg(self):
         with self.assertRaises(SystemExit):
-            logmunger.parse_args(['--doslog', 'somefile.txt'])
+            logmunger.parse_args(
+                ['--doslog', 'tests/fixtures/good_dos_log.csv'])
 
     def test_no_exit_with_sfslog_and_doslog_args(self):
         try:
             logmunger.parse_args(
-                ['--sfslog', 'somefile.txt',
-                 '--doslog', 'somefile.txt'])
+                ['--sfslog', 'tests/fixtures/good_sfs_log.txt',
+                 '--doslog', 'tests/fixtures/good_dos_log.csv'])
         except SystemExit:
             self.fail('SystemExit raised unexpectedly!')
 
     def test_both_filename_argument_parsed(self):
         parser = logmunger.parse_args(
-                    ['--sfslog', 'sfslog.txt',
-                     '--doslog', 'doslog.csv'])
-        self.assertEqual('sfslog.txt', parser.sfslog)
-        self.assertEqual('doslog.csv', parser.doslog)
+                    ['--sfslog', 'tests/fixtures/good_sfs_log.txt',
+                     '--doslog', 'tests/fixtures/good_dos_log.csv'])
+        self.assertEqual('tests/fixtures/good_sfs_log.txt', parser.sfslog.name)
+        self.assertEqual('tests/fixtures/good_dos_log.csv', parser.doslog.name)
