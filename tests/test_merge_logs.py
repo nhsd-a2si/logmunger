9b41bb5eb2c7441f5182abb908030fa7e628618a
Mark Henwood <mcbhenwood@gmail.com>
2018-12-20 14:02:05 +0000

    Improve test legibility

--- tests/test_merge_logs.py
+++ tests/test_merge_logs.py
@@ -3,6 +3,11 @@ import unittest
 
 from .context import logmunger
 
+TIMESTAMP_1 = datetime.datetime(year=2018, month=12, day=7,
+                                hour=12, minute=3, second=3)
+TIMESTAMP_2 = datetime.datetime(year=2018, month=12, day=7,
+                                hour=12, minute=5, second=9)
+
 
 class MergeLogsTestCase(unittest.TestCase):
     def test_empty_logs_return_empty_merge(self):
@@ -10,84 +15,95 @@ class MergeLogsTestCase(unittest.TestCase):
         self.assertEqual({}, result)
 
     def test_empty_dos_log_yields_unchanged_sfs_log(self):
-        sfs_log = {datetime.datetime(year=2018, month=12, day=7,
-                                     hour=12, minute=3, second=3):
-                       {'postcode': 'W1A 1AA'}}
+        sfs_log = {
+            TIMESTAMP_1: {
+                'postcode': 'W1A 1AA'
+            }
+        }
         result = logmunger.merge_logs(sfs_log=sfs_log, dos_log={})
-        self.assertEqual(sfs_log, result)
+        self.assertEqual(
+            {
+                TIMESTAMP_1: {
+                    'postcode': 'W1A 1AA'
+                }
+            },
+            result
+        )
 
     def test_matching_dos_records_augment_sfs_log(self):
-        sfs_log = {datetime.datetime(year=2018, month=12, day=7,
-                                     hour=12, minute=3, second=3):
-                       {'postcode': 'W1A 1AA'},
-                   datetime.datetime(year=2018, month=12, day=7,
-                                     hour=12, minute=5, second=9):
-                       {'postcode': 'W1B 1BB'}
-                   }
+        sfs_log = {
+            TIMESTAMP_1: {
+                'postcode': 'W1A 1AA'
+            },
+            TIMESTAMP_2: {
+                'postcode': 'W1B 1BB'
+            }
+        }
         dos_log = {
-            datetime.datetime(year=2018, month=12, day=7,
-                              hour=12, minute=5, second=9):
-                {'pilot_id': '12345',
-                 'role': 'Admin',
-                 'result_count': 22,
-                 'status': 'active',
-                 'dos_region_name': 'A Region'}
+            TIMESTAMP_2: {
+                'pilot_id': '12345',
+                'role': 'Admin',
+                'result_count': 22,
+                'status': 'active',
+                'dos_region_name': 'A Region'
+            }
         }
         result = logmunger.merge_logs(sfs_log=sfs_log, dos_log=dos_log)
         self.assertEqual(
             {
-                datetime.datetime(year=2018, month=12, day=7,
-                                  hour=12, minute=3, second=3):
-                    {'postcode': 'W1A 1AA'},
-                datetime.datetime(year=2018, month=12, day=7,
-                                  hour=12, minute=5, second=9):
-                    {'postcode': 'W1B 1BB', 'pilot_id': '12345',
-                     'role': 'Admin',
-                     'result_count': 22,
-                     'status': 'active',
-                     'dos_region_name': 'A Region'}
+                TIMESTAMP_1: {
+                    'postcode': 'W1A 1AA'
+                },
+                TIMESTAMP_2: {
+                    'postcode': 'W1B 1BB', 'pilot_id': '12345',
+                    'role': 'Admin',
+                    'result_count': 22,
+                    'status': 'active',
+                    'dos_region_name': 'A Region'
+                }
             },
             result
         )
 
     def test_source_dicts_unaltered(self):
-        sfs_log = {datetime.datetime(year=2018, month=12, day=7,
-                                     hour=12, minute=3, second=3):
-                       {'postcode': 'W1A 1AA'},
-                   datetime.datetime(year=2018, month=12, day=7,
-                                     hour=12, minute=5, second=9):
-                       {'postcode': 'W1B 1BB'}
-                   }
+        sfs_log = {
+            TIMESTAMP_1: {
+                'postcode': 'W1A 1AA'
+            },
+            TIMESTAMP_2: {
+                'postcode': 'W1B 1BB'
+            }
+        }
         dos_log = {
-            datetime.datetime(year=2018, month=12, day=7,
-                              hour=12, minute=5, second=9):
-                {'pilot_id': '12345',
-                 'role': 'Admin',
-                 'result_count': 22,
-                 'status': 'active',
-                 'dos_region_name': 'A Region'}
+            TIMESTAMP_2: {
+                'pilot_id': '12345',
+                'role': 'Admin',
+                'result_count': 22,
+                'status': 'active',
+                'dos_region_name': 'A Region'
+            }
         }
         logmunger.merge_logs(sfs_log=sfs_log, dos_log=dos_log)
         self.assertEqual(
             {
-                datetime.datetime(year=2018, month=12, day=7,
-                                  hour=12, minute=3, second=3):
-                    {'postcode': 'W1A 1AA'},
-                datetime.datetime(year=2018, month=12, day=7,
-                                  hour=12, minute=5, second=9):
-                    {'postcode': 'W1B 1BB'}
+                TIMESTAMP_1: {
+                    'postcode': 'W1A 1AA'
+                },
+                TIMESTAMP_2: {
+                    'postcode': 'W1B 1BB'
+                }
             },
             sfs_log
         )
         self.assertEqual(
             {
-                datetime.datetime(year=2018, month=12, day=7,
-                                  hour=12, minute=5, second=9):
-                    {'pilot_id': '12345',
-                     'role': 'Admin',
-                     'result_count': 22,
-                     'status': 'active',
-                     'dos_region_name': 'A Region'}
+                TIMESTAMP_2: {
+                    'pilot_id': '12345',
+                    'role': 'Admin',
+                    'result_count': 22,
+                    'status': 'active',
+                    'dos_region_name': 'A Region'
+                }
             },
             dos_log
         )
